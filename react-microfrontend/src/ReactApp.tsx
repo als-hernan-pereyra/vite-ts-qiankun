cc224f8e996c8da47454728abbf5c60c40b983fb
Hernán Pereyra <hernan.pereyra@alsacia.com.uy>
2025-04-03 11:49:47 -0300

    creo react mfe

---
+++ react-microfrontend/src/ReactApp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReactApp = () => {
+  return <div>Microfrontend React</div>;
+};
+
+export default ReactApp;
---
+++ react-microfrontend/vite.config.ts
@@ -0,0 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/ReactApp.tsx',
+      name: 'ReactApp',
+      fileName: 'reactApp',
+    },
+    rollupOptions: {
+      output: {
+        format: 'system',
+        entryFileNames: 'reactApp.js',
+      },
+    },
+  },
+});
